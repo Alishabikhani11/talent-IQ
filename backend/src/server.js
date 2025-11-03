b831d220d68f4fc31e8befc0e0952691bb311e02
alisha <alishabikhani@gmail.com>
2025-11-04 01:29:39 +0530

    imp

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,21 @@
 import express from "express"
 import { ENV } from "./lib/env.js"
+import connectDB from "./lib/db.js";
 import path from "path"
+import cors from "cors"
+import {serve} from "inngest/express"
 
 const app=express();
 
 const __dirname=path.resolve();
+
+//middleware
+app.use(express.json());
+
+//credential:true?? means server allows browser(frontend) to include cookies at req
+app.use(cors({origin:ENV.CLIENT_URL,credential:true}));
+
+app.use("/api/inngest",serve({client: inngest,functions}));
 
 app.get('/health',(req,res)=>{
     res.status(200).json({"msg":"api is running and up"});
