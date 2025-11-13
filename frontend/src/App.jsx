b77f32ce0b0aaaa5f78d4d9dea3fe2b35a2e5aec
alisha <alishabikhani@gmail.com>
2025-11-13 22:50:52 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 
 import { Toaster } from "react-hot-toast";
-import DashboardPage from "./pages/DashBoardPage";
+import DashBoardPage from "./pages/DashBoardPage";
 import ProblemPage from "./pages/ProblemPage";
 import ProblemsPage from "./pages/ProblemsPage";
 import SessionPage from "./pages/SessionPage";
@@ -18,7 +18,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
-        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashBoardPage /> : <Navigate to={"/"} />} />
 
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
         <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
