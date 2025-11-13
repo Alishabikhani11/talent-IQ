555af610234a8c3fc45c9253793ef407b740ff2b
alisha <alishabikhani@gmail.com>
2025-11-13 22:41:01 +0530

    complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,33 @@
-import { SignInButton,SignedOut,SignedIn,SignOutButton,UserButton ,useUser} from '@clerk/clerk-react'
-import{Navigate,Route,Routes} from "react-router"
+import { useUser } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
-import DashBoardPage from "./pages/DashBoardPage";
+
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
+import ProblemPage from "./pages/ProblemPage";
 import ProblemsPage from "./pages/ProblemsPage";
-import ProblemPage from "./pages/problemPage";
-import { Toaster } from 'react-hot-toast';
-
-
+import SessionPage from "./pages/SessionPage";
 
 function App() {
-  
-  const {isSignedIn,isLoaded}=useUser();
+  const { isSignedIn, isLoaded } = useUser();
 
-  //this will get rid of flickering effect
-  if(!isLoaded) return null;
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
+
   return (
     <>
-    <Routes>
-       <Route path="/" element={!isSignedIn ? <HomePage />:<Navigate to ={"/dashboard"}/>}/>
-       <Route path="/dashboard" element={ <DashBoardPage />}/>
-       
+      <Routes>
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
 
-       <Route path="/problems" element={ <ProblemsPage /> }/>   
-       <Route path="/problem/:id" element={!isSignedIn ? <ProblemPage />:<Navigate to ={"/"}/>}/>  
-    </Routes>
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+        <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
+      </Routes>
 
-    <Toaster toastOptions={{duration:3000}}/>
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
-    
-  )
+  );
 }
 
 export default App;
-
-//tw,daisyui.react-router,react-hot-toast
-// todo:react-query aka tanstack query,axios
